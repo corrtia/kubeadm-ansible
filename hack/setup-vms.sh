45ebe9cb9cb005f50e74cd2c07915c962b7e5437
corrtain@gmail.com <huqc@zhejianglab.com>
2023-08-30 15:13:12 +0800

    更改国内源

--- hack/setup-vms.sh
+++ hack/setup-vms.sh
@@ -18,10 +18,10 @@ EOF
 
 set -e
 HOST_NAME=$(hostname)
-OS_NAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release | grep -o "\w*"| head -n 1)
+os_distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release | grep -o "\w*"| head -n 1)
 
 if [ ${HOST_NAME} == "k8s-m1" ]; then
-  case "${OS_NAME}" in
+  case "${os_distro}" in
     "CentOS")
       sudo yum install -y epel-release
       sudo yum install -y git ansible sshpass python-netaddr openssl-devel
@@ -32,7 +32,7 @@ if [ ${HOST_NAME} == "k8s-m1" ]; then
       sudo apt-get update && sudo apt-get install -y ansible git sshpass python-netaddr libssl-dev
     ;;
     *)
-      echo "${OS_NAME} is not support ..."; exit 1
+      echo "${os_distro} is not support ..."; exit 1
   esac
 
   yes "/root/.ssh/id_rsa" | sudo ssh-keygen -t rsa -N ""
